7ab1ca0b0ec14be00827c6677f262f463ad15e86
Matheus Souza <Matheus.Souza-ext2@ab-Inbev.com>
2024-01-05 14:30:57 -0300

    feat(promises): refactor code using promises

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,15 +1,19 @@
 /*
-
-0 Obter usuario
-1 Obter o numero de telefone de um usuario a partir de seu ID
-2 obter o endereco do usuário pelo Id
-
+  0 Obter usuario
+  1 Obter o numero de telefone de um usuario a partir de seu ID
+  2 obter o endereco do usuário pelo Id
 */
 
 /*
   quando der algum problema -> reject(ERRO)
   quando sucess -> RESOLVE
 */
+
+// importamos um modulo interno do node.js
+import util from 'util';
+
+const obterEnderecoAsync = util.promisify(obterEndereco);
+
 function obterUsuario() {
   return new Promise(function resolvePromise(resolve, reject) {
     setTimeout(function () {
@@ -27,7 +31,7 @@ function obterTelefone(idUsuario) {
     setTimeout(() => {
       return resolve({
         telefone: "71080702929",
-        dd: 71,
+        ddd: 71,
       });
     }, 2000);
   }) 
@@ -49,44 +53,71 @@ function obterEndereco(idUsuario, callback) {
 const usuarioPromise = obterUsuario();
 usuarioPromise
   .then(function (usuario) {
-    return obterTelefone(usuario.id);
+    return obterTelefone(usuario.id)
+          .then(function resolverTelefone(result){
+              return {
+                  usuario: {
+                      nome: usuario.nome,
+                      id: usuario.id
+                  },
+                  telefone: result
+              }
+          })
   })
   .then(function (resultado) {
-    console.log("resultado ", resultado);
+    const endereco = obterEnderecoAsync(resultado.usuario.id);
+    return endereco.then(function resolverEndereco(result) {
+      return {
+        usuario: {
+          nome: resultado.usuario.nome,
+          id: resultado.usuario.id,
+        },
+        telefone: resultado.telefone,
+        endereco: result,
+      };
+    });
+  })
+  .then(function (resultado) {
+    console.log(`
+      Nome: ${resultado.usuario.nome}
+      Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+      Telefone: (${resultado.telefone.ddd} ${resultado.telefone.telefone})
+    `);
   })
   .catch(function (error) {
     console.error("DEU RUIM", error);
   });
 
-// function resolverUsuario(erro, usuario) {
-//     console.log('usuario', usuario);
-// }
+/*
+function resolverUsuario(erro, usuario) {
+    console.log('usuario', usuario);
+}
 
-// obterUsuario( function resolverUsuario(error, usuario) {
+obterUsuario( function resolverUsuario(error, usuario) {
 
-//     if (error) {
-//         console.error ('DEU RUIM em USUARIO', error);
-//         return;
-//     }
+    if (error) {
+        console.error ('DEU RUIM em USUARIO', error);
+        return;
+    }
 
-//     obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
-//         if (error) {
-//             console.error ('DEU RUIM em TELEFONE', error1);
-//             return;
-//         }
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+        if (error) {
+            console.error ('DEU RUIM em TELEFONE', error1);
+            return;
+        }
 
-//         obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
-//             if (error) {
-//                 console.error ('DEU RUIM em ENDERECO', error2);
-//                 return;
-//             }
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if (error) {
+                console.error ('DEU RUIM em ENDERECO', error2);
+                return;
+            }
 
-//             console.log(`
-//                 Nome: ${usuario.nome},
-//                 Endereco: ${endereco.rua}, ${endereco.numero},
-//                 Telefone: ${telefone.dd}, ${telefone.telefone}
-//             `)
-//         });
-//     });
-// });
- ,
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereco: ${endereco.rua}, ${endereco.numero},
+                Telefone: ${telefone.dd}, ${telefone.telefone}
+            `)
+        });
+    });
+});
+*/
