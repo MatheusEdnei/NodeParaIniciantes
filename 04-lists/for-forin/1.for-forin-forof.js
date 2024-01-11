fe57d8d58873275c8c74e130d465a62ca97b8f19
Matheus Souza <Matheus.Souza-ext2@ab-Inbev.com>
2024-01-11 15:31:28 -0300

    feat(for): add code using for, forIn and forOf

--- 04-lists/for-forin/1.for-forin-forof.js
+++ 04-lists/for-forin/1.for-forin-forof.js
@@ -5,12 +5,26 @@ async function main() {
         const result = await obterPessoas('a');
         const names = [];
 
+        console.time('for');
         for (let i = 0; i<= result.results.length - 1; i++) {
             const pessoa = result.results[i];
             names.push(pessoa.name);
         }
+        console.timeEnd('for');
 
-        console.log('names ', names);
+        console.time('forIn');
+        for (let i in result.results) {
+            const pessoa = result.results[i];
+            names.push(pessoa.name);
+        }
+        console.timeEnd('forIn');
+
+        console.time('forOf');
+        for (pessoa of result.results) {
+            names.push(pessoa.name);
+        }
+        console.timeEnd('forOf');
+        
     } catch (error) {
         console.error(`error interno`, error);
     }
